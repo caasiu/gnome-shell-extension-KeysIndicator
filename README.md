c674b75f2428360801e3353d2e7cead2b2b27055
caasiu <Rogers619952467@gmail.com>
2016-08-12 17:27:37 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# gnome-shell-extension-KeysIndicator
+
+It shows <kbd>Ctrl</kbd> <kbd>Shift</kbd> <kbd>Alt</kbd> <kbd>CapsLock</kbd> <kbd>NumLock</kbd> status on the panel when these keys are pressed.
+
+This extension supports gnome-shell 3.18 & 3.20
+
+To install this extension, copy keys-indicator@caasiu.github.com to the ```~/.local/share/gnome-shell/extensions/ ```
+
+
+Another more advance and beautiful CapsLock&NumLock extionsion [lockkeys@vaina.lt](https://github.com/kazysmaster/gnome-shell-extension-lockkeys)
---
+++ keys-indicator@caasiu.github.com/extension.js
@@ -0,0 +1,170 @@
+
+const St = imports.gi.St;
+const Lang = imports.lang;
+const Clutter = imports.gi.Clutter;
+const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
+const Atk = imports.gi.Atk;
+
+//detect the keyboard key press event
+const Gdk = imports.gi.Gdk;
+const Keymap = Gdk.Keymap.get_default();
+
+//const ExtensionUtils = imports.misc.extensionUtils;
+//const Me = ExtensionUtils.getCurrentExtension();
+//const Convenience = Me.imports.convenience;
+
+const Panel = imports.ui.panel;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+
+const Gettext = imports.gettext;
+const _ = Gettext.gettext;
+
+let keysIndicator;
+
+
+const KeysIndicator = new Lang.Class({
+    Name: 'KeysIndicator',
+    Extends: PanelMenu.Button,
+
+    _init: function(){
+        this.parent(0.0, _("KeysIndicator"), true);
+
+        //define 5 label to show the keys status later
+        this.capsLock = new St.Label({ style_class: "label-style",
+                                       y_align: Clutter.ActorAlign.CENTER,
+                                       visible: false,
+                                       text: _("A") });
+
+        this.numLock = new St.Label({ style_class: "label-style",
+                                      y_align: Clutter.ActorAlign.CENTER,
+                                      visible: false,
+                                      text: _("1") });
+
+        this.keyCtrl = new St.Label({ style_class: "label-style",
+                                      y_align: Clutter.ActorAlign.CENTER,
+                                      visible: false,
+                                      text: _("Ctrl") });
+        
+        this.keyShift = new St.Label({ style_class: "label-style",
+                                      y_align: Clutter.ActorAlign.CENTER,
+                                      visible: false,
+                                      text: _("Shift") });
+
+        this.keyAlt = new St.Label({ style_class: "label-style",
+                                     y_align: Clutter.ActorAlign.CENTER,
+                                     visible: false,
+                                     text: _("Alt") });
+
+        //put all the label together
+        //this.layoutManager = new St.BoxLayout({veritcal: false, style_class: 'box-style'});
+        this.layoutManager = new St.BoxLayout({style_class: 'box-style'});
+        //Todo: add 5 label using one line?
+        this.layoutManager.add(this.capsLock);
+        this.layoutManager.add(this.numLock);
+        this.layoutManager.add(this.keyCtrl);
+        this.layoutManager.add(this.keyShift);
+        this.layoutManager.add(this.keyAlt);
+
+        //add the box to the panel 
+        this.actor.add_actor(this.layoutManager);
+        //this.actor.visible = false;
+        Keymap.connect('state_changed', Lang.bind(this, this._updateStatus));
+    },
+
+    setActive: function(enable){
+        if (enable){
+            this.actor.visible = true;
+            this._KeyStatusId = Keymap.connect('state_changed', Lang.bind(this, this._updateStatus));
+        } else {
+            this.actor.visible = false;
+            Keymap.disconnect(this._KeyStatusId);
+        }
+    },
+
+    _updateStatus: function(){
+        let capStatus = Keymap.get_caps_lock_state();
+        let numStatus = Keymap.get_num_lock_state();
+
+        //get_modifier_state will return a num refer to the key
+        //CapsLock is 2; NumLock is 16; Shift is 1; Ctrl is 4; Alt is 8
+        //every time we press these keys will be the numbers add
+        //example <Ctrl>+<Shift> is 5
+        let keyStatusCode = Keymap.get_modifier_state();
+
+        if (capStatus){
+            this.capsLock.visible = true;
+            keyStatusCode = keyStatusCode - 2;
+        } else {
+            this.capsLock.visible = false;
+        }
+
+        if (numStatus){
+            this.numLock.visible = true;
+            keyStatusCode = keyStatusCode - 16;
+        } else {
+            this.numLock.visible = false;
+        }
+
+        switch(keyStatusCode){
+            case 1:
+                this.keyShift.visible = true;
+                break;
+            case 4:
+                this.keyCtrl.visible = true;
+                break;
+            case 8:
+                this.keyAlt.visible = true;
+                break;
+            case 5:
+                this.keyCtrl.visible = true;
+                this.keyShift.visible = true;
+                break;
+            case 9:
+                this.keyAlt.visible = true;
+                this.keyShift.visible = true;
+                break;
+            case 12:
+                this.keyCtrl.visible = true;
+                this.keyAlt.visible = true;
+                break;
+            case 13:
+                this.keyCtrl.visible = true;
+                this.keyAlt.visible = true;
+                this.keyShift.visible = true;
+                break;
+            default:
+                this.keyCtrl.visible = false;
+                this.keyAlt.visible = false;
+                this.keyShift.visible = false;
+        }
+    },
+
+    destroy: function(){
+        //this.setActive(false);
+        this.parent();
+    },
+
+});
+
+
+function init(){
+
+}
+
+
+function enable(){
+
+    keysIndicator = new KeysIndicator;
+    //keysIndicator.setActive(true);
+
+    //the number '2' is for position
+    //0 is left(default); 1 is middle; 2 is right;
+    Main.panel.addToStatusArea('keysIndicator', keysIndicator, 2);
+}
+
+
+function disable(){
+    keysIndicator.destroy();
+}
